b50470e3be2854b6af1ba155d2f922553f2d2798
Reza Shahriari <Rezakingdom@gmail.com>
2021-02-23 13:41:58 -0500

    changed invalid url in stripe cancel url

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     success_url: `${req.protocol}://${req.get('host')}/?tour=${tour.id}&user=${
       req.user.id
     }&price=${tour.price}`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
     line_items: [
